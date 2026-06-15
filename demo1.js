fcb71e12210ea11f69d1ba2835f0c485055f3939
veera ariya <veeraariya.dct@gmail.com>
2026-06-15 09:47:14 +0530

    test

---
+++ demo1.js
@@ -0,0 +1,8 @@
+function add(){
+const input = document.getElementById("inp1").value;
+const input1 = document.getElementById("inp").value;
+ let num1 = Number(input);
+ let num2 = Number(input1 );
+let ad=num1+num2
+console.log(ad)
+}
